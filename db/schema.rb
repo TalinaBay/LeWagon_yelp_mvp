bfbae8bfbbd3b22437b2979a9685cc77f98f9f5c
Talina Bay <talinabayeleva@gmail.com>
2021-06-15 20:34:25 +1000

    updated Review : added a new column - rating

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_15_094107) do
+ActiveRecord::Schema.define(version: 2021_06_15_101801) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_06_15_094107) do
     t.integer "restaurant_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "rating"
     t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
